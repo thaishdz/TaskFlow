c8411fe6b0be99d2907d68dfb65c855d71d06d8c
Thais <thaishdzlpz@gmail.com>
2025-12-01 17:07:56 +0000

    feat(ui): add Dialog component with responsive design

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,37 @@
+import { useState } from "react"
 import { Button } from "./components/ui/Button"
 import { Card } from "./components/ui/Card"
+import { Dialog } from "./components/ui/Dialog"
+
+
 
 function App() {
+  const [isDialogOpen, setIsDialogOpen] = useState(false)
+  
+  const openDialog = () => setIsDialogOpen(true)
+  const closeDialog = () => setIsDialogOpen(false)
+
   return (
     <>
-      <h1 className="text-4xl font-thin text-center mt-35">My Task Board</h1>
-      <div>
+      <h1 className="text-4xl font-thin text-center mt-20">Board</h1>
+      <div className="relative px-4 mt-8">
+          <Button 
+            onClick={openDialog}
+            className="absolute top-0 right-4 md:right-96"
+          >
+            Add list
+          </Button>
+        <div className="pt-16">
         {
           Array.from({length: 4}).map((_, index) => (
             <Card key={index} />
           ))
         }
+        </div>
       </div>
-      <div className="flex justify-end border">
-        <Button />
-      </div>
+      <Dialog visible={isDialogOpen} onClose={closeDialog} >
+        <p>Aquí irá el formulario</p>
+      </Dialog>
     </>
   )
 }
